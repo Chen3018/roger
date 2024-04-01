438b7681e14f163d219424b58d3c2e441625c821
cathyxfeiyang <157295142+cathyxfeiyang@users.noreply.github.com>
2024-04-01 18:14:58 -0400

    Added unit test

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,5 +1,7 @@
+import vertexai
 from src.translator import translate_content
-
+from mock import patch
+from typing import Callable
 
 def test_chinese():
     is_english, translated_content = translate_content("这是一条中文消息")
@@ -18,3 +20,14 @@ def test_llm_gibberish_response():
     is_english, translated_content = translate_content("guhjbnh ghjbnhukyhj hu")
     assert is_english == True
     assert translated_content == "guhjbnh ghjbnhukyhj hu"
+
+@patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+def test_unexpected_language(mocker):
+    mocker.return_value.text = "I don't understand your request"
+    content = "Aquí está su primer ejemplo."
+    assert translate_content(content) == (True, content)
+    mocker.return_value.text = ""
+    assert translate_content(content) == (True, content)
+    mocker.return_value.text = {}
+    assert translate_content(content) == (True, content)
+
