86ab89033c382299d479b2bf2d4ed92da5e40e88
Chen3018 <chenjia@LAPTOP-A7QR1HLO>
2024-04-04 13:04:42 -0400

    Added Cathy's changes

--- test/unit/test_translator.py
+++ test/unit/test_translator.py
@@ -1,5 +1,7 @@
+import vertexai
 from src.translator import translate_content
-
+from mock import patch
+from typing import Callable
 
 def test_chinese():
     is_english, translated_content = translate_content("这是一条中文消息")
@@ -7,7 +9,25 @@ def test_chinese():
     assert translated_content == "This is a Chinese message"
 
 def test_llm_normal_response():
-    pass
+    is_english, translated_content = translate_content("Ceci est un message en français")
+    assert is_english == False
+    assert translated_content == "This is a French message"
+    is_english, translated_content = translate_content("This is an English message")
+    assert is_english == True
+    assert translated_content == "This is an English message"
 
 def test_llm_gibberish_response():
-    pass
+    is_english, translated_content = translate_content("guhjbnh ghjbnhukyhj hu")
+    assert is_english == True
+    assert translated_content == "guhjbnh ghjbnhukyhj hu"
+
+@patch('vertexai.preview.language_models._PreviewChatSession.send_message')
+def test_unexpected_language(mocker):
+    mocker.return_value.text = "I don't understand your request"
+    content = "Aquí está su primer ejemplo."
+    assert translate_content(content) == (True, content)
+    assert translate_content("??????????") == (True, "??????????")
+    mocker.return_value.text = ""
+    assert translate_content(content) == (True, content)
+    mocker.return_value.text = {}
+    assert translate_content(content) == (True, content)
