44de7fbba1a39174e55300228793b8a7c8c35cc9
Chen3018 <chenjia@LAPTOP-A7QR1HLO>
2024-04-04 13:29:29 -0400

    Changed google colab dependent code

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,14 @@
 from typing import Callable
-from google.colab import auth
 from google.cloud import aiplatform, storage
 
 PROJECT_ID = "active-smile-416914"
 
 client = storage.Client(project=PROJECT_ID)
 
-auth.authenticate_user()
+credentials,_ = default()
 
 aiplatform.init(
     project=PROJECT_ID,
-
     location='us-central1',
+    credentials=credentials
 )
