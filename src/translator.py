7ca12fd7f1f2ed0451453cf484a7849cb590da7c
Chen3018 <chenjia@LAPTOP-A7QR1HLO>
2024-04-04 15:56:24 -0400

    Moved chat model

--- src/translator.py
+++ src/translator.py
@@ -1,9 +1,7 @@
 from vertexai.preview.language_models import ChatModel, InputOutputTextPair
 
-chat_model = ChatModel.from_pretrained("chat-bison@001")
-
 def get_translation(post: str) -> str:
-    global chat_model
+    chat_model = ChatModel.from_pretrained("chat-bison@001")
     context_text = "You are a translator. If given a sentence not in English, translate it to English. If the sentence is already in English, return the same sentence."
 
     parameters = {
@@ -16,7 +14,7 @@ def get_translation(post: str) -> str:
     return response.text
 
 def get_language(post: str) -> str:
-    global chat_model
+    chat_model = ChatModel.from_pretrained("chat-bison@001")
     context_lang = "You are a translator. Your job is to classify the language of a piece of text."
 
     parameters = {
