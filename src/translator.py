d935962b9514d6455c84fe94ae00be0d645c5815
Chen3018 <chenjia@LAPTOP-A7QR1HLO>
2024-04-04 17:39:38 -0400

    Removed unused dependencies

--- src/translator.py
+++ src/translator.py
@@ -1,9 +1,13 @@
 from vertexai.preview.language_models import ChatModel, InputOutputTextPair
 
-chat_model = ChatModel.from_pretrained("chat-bison@001")
+chat_model = None
 
 def get_translation(post: str) -> str:
     global chat_model
+
+    if chat_model == None:
+        chat_model = ChatModel.from_pretrained("chat-bison@001")
+
     context_text = "You are a translator. If given a sentence not in English, translate it to English. If the sentence is already in English, return the same sentence."
 
     parameters = {
@@ -17,6 +21,10 @@ def get_translation(post: str) -> str:
 
 def get_language(post: str) -> str:
     global chat_model
+
+    if chat_model == None:
+        chat_model = ChatModel.from_pretrained("chat-bison@001")
+        
     context_lang = "You are a translator. Your job is to classify the language of a piece of text."
 
     parameters = {
